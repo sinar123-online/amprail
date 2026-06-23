4cce60e07865bbece2cf5b4bc099c5fb6f999a22
sinar123-online <gscsinar123@gmail.com>
2026-06-24 06:33:16 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
